8137c757090a8d1cb76d88df6e01407e51667de4
Anton Timenko <a@timenko.com>
2015-10-05 20:23:06 +0300

    Added option for collectstatic

--- rozklad/settings.py
+++ rozklad/settings.py
@@ -2,7 +2,6 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
@@ -106,3 +105,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
